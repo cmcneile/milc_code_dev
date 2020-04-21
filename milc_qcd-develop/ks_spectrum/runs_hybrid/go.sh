cef1d75bea47ff34b4bc1193b5d29b21b17ea3fd
Gaurav Ray <gaurav.sinharay@plymouth.ac.uk>
2020-04-21 19:22:50 +0100

    cleaning up filesystem

--- milc_qcd-develop/ks_spectrum/runs_hybrid/go.sh
+++ milc_qcd-develop/ks_spectrum/runs_hybrid/go.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 
-rm -f  ks_spectrum_hisq.hybrid_corr
+rm -f  ks_spectrum_naive.hybrid_corr
 
-##../ks_spectrum_hisq  ks_spectrum_hisq.nl.2.sample-in
 
-##../ks_spectrum_hisq ks_spectrum_hisq.spectrum2.2.sample-in 
-
-../ks_spectrum_naive hybrid-in-naive
-
-##../ks_spectrum_hisq hybrid-inA
-
+../ks_spectrum_naive templates/hybrid-in-naive
 
 
 exit 0
